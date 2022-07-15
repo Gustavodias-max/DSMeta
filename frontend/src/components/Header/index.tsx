5dd1a00993deb6815f013d44dd211b7249fe380c
Gustavo Dias <gustavoddsantos@outlook.com>
2022-07-15 12:00:32 -0300

    update

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ function Header() {
             <div className="dsmeta-logo-conteiner">
                 <img src={logo} alt="DSMeta" />
                     <h1>DSMeta</h1>
-                    <p>Desenvolvido por <a href="https://www.youtube.com">Gustavo Dias</a> </p>
+                    <p>Desenvolvido por <a href="https://github.com/GustavoDias-max">Gustavo Dias</a> </p>
             </div>
         </header>
     )
